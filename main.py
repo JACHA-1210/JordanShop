aaf41b663be13573a947f55f2dd6a4690d1c3e75
Jose Andres Chero Ascurra <jose_andreschero@ceroca.cat>
2023-05-03 19:14:15 +0200

    Commit: falten img

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+products = [
+    {
+        "name": "Air Jordan 1 Retro High OG",
+        "image": "img/air-jordan-1.jpg",
+        "description": "Las Air Jordan 1 Retro High OG son las zapatillas que empezaron todo en 1985. La versión actual conserva el diseño icónico de alta calidad de las originales y añade características modernas para una mayor comodidad y durabilidad.",
+        "price": "200€"
+},
+{
+"name": "Air Jordan 1 Mid",
+"image": "img/air-jordan-mid.jpg",
+"description": "Las Air Jordan 1 Mid están confeccionadas con materiales de alta calidad y ofrecen una comodidad excepcional. Estas zapatillas son perfectas para el uso diario y para cualquier ocasión.",
+"price": "110€"
+}
+]
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', products=products)
 
 @app.route('/air-jordan-1')
 def air_jordan_1():
