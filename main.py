4645f0a96fc1ebfbcfafbb96b0bae0d034311aa7
Joel Treboles <j.treboles.pilataxi@gmail.com>
2023-05-10 16:48:53 +0200

    Commit 6: modificando la pagina

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ products = [
 "price": "110€"
 },
 {
-"name": "Air Jordan 1 Mid",
-"image": "img/air-jordan-mid.jpg",
-"description": "Las Air Jordan 1 Mid están confeccionadas con materiales de alta calidad y ofrecen una comodidad excepcional. Estas zapatillas son perfectas para el uso diario y para cualquier ocasión.",
+"name": "Jordan 5 Retro Off-White",
+"image": "img/jordan-retro-5.jpg",
+"description": "Este lanzamiento rinde homenaje a los primeros Jordan que Virgil tuvo, los Air Jordan 5. De una manera tradicional, Virgil rediseñó esta silueta clásica subrayando la importancia de la tecnología Nike Air.Estos Jordan 5 están confeccionados con un upper de malla Sail con paneles de red translúcidos e inserciones por todo el sneaker.",
 "price": "110€"
 }
 ]
